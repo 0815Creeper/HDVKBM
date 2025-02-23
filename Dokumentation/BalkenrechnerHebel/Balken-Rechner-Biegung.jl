e5315932be4b99d3aa5eb766d59d041ddc451a44
exnersim <simon.exner@student.uni-augsburg.de>
2025-02-23 14:38:59 +0100

    zwischenstand

--- Dokumentation/BalkenrechnerHebel/Balken-Rechner-Biegung.jl
+++ Dokumentation/BalkenrechnerHebel/Balken-Rechner-Biegung.jl
@@ -9,142 +9,53 @@
 #  |         |
 #   ---------
 #     --A--
-
-function get_area(A, B, C, D, profile)
-	if contains(profile, "H-Profil") || contains(profile, "IPE-Profil")
-		return (A * D * 2 + (B - 2 * D) * C)
-	elseif contains(profile, "4-Kant")
-		return A * D * 2 + (B - 2 * D) * C * 2
-	else
-		throw(ArgumentError(string("\"", profile, "\" is not a known profile")))
-	end
-end
-
-function get_widerstandsmoment(A, B, C, D, profile)
-	if contains(profile, "H-Profil") || contains(profile, "IPE-Profil")
-		return (A * B^3 - (A - C) * (B - 2 * D)^3) / (6 * B)
-	elseif contains(profile, "4-Kant")
-		return (A * B^3 - (A - 2 * C) * (B - 2 * D)^3) / (6 * B)
-	else
-		throw(ArgumentError(string("\"", profile, "\" is not a known profile")))
-	end
-end
-
-l = 180 # siehe bedienhebel_festigkeit.png für eine Skizze
-a = 32.3 # siehe bedienhebel_festigkeit.png für eine Skizze
-
-A = get_area(20, 20, 2, 2, "4-Kant")
-W = get_widerstandsmoment(20, 20, 2, 2, "4-Kant")
-E = 70000
-σ = 145
 F = 500
+wandstrke = 2
+breite = 20
+l = 180
+A = 4 * wandstrke * breite - 4 * wandstrke * wandstrke
+y_SP = breite / 2
+z_SP = breite / 2
+I_y = (breite^4.0 - (breite - 2 * wandstrke)^4.0) / 12.0
+W_y = I_y * 2.0 / breite
 
-function get_lager_reaktionen(F, a, l)
-	# Fliegende Einzellast mit Festlager A (Hebel Drehpunkt) und Loaslager B (Anschlag unten am Schlitten)
-	F_F_A = F * (l/a - 1)
-	F_F_B = F - F_F_A
-	F_M_A = 0
-	F_M_B = 0
-end
+x_values = 0.00:0.01:l
+a = 32.3
+# maximale Biegespannung 
+M_b_max = 0
+x_M_b_max = 0.00
 
-x_max_biege_M = 
-M_max = 
-σ_max = 
-SF = 
-for i in eachindex(experiments)
-	global a_i = 1
-	global a_max = 1
-	push!(lagerreaktionen, Vector())
-	push!(x_max_biege_M, zeros(length(experiments)))
-	push!(M_max, zeros(length(experiments)))
-	push!(σ_max, zeros(length(experiments)))
-	push!(SF, zeros(length(experiments)))
-	local F, ρ, A, l, W, E, σ_grenze = experiments[i]
-	for a in 0:0.02:(l+0.01)
-		push!(lagerreaktionen[i], get_lager_reaktionen((F, a, ρ, A, l)))
-		local q = ρ * A * 1e-3 * g # *l/l
-		local F_A, F_B, M_A, M_B = lagerreaktionen[i][a_i]
-		# always output abs max M 
-		M =
-			x -> (
-				(x > a) ? (F_A * x - M_A - q * x * x / 2 - F * (x - a)) :
-				((x < a) ? (F_A * x - M_A - q * x * x / 2) :
-				 ([F_A * x - M_A - q * x * x / 2, F_A * x - M_A - q * x * x / 2 - F * (x - a)][argmax(abs.([F_A * x - M_A - q * x * x / 2, F_A * x - M_A - q * x * x / 2 - F * (x - a)]))])
-				)
+# lagerreaktionen
+F_A = F * (l / a - 1)
+F_B = F * l / a
+#M_lcl = x -> (F_A * x + F_B * (a - x) + F * (x - l))
+for x in x_values
+	global M_b_max
+	global x_M_b_max
+
+	M_lcl =
+		x -> (
+			(x < a) ? (F_A * x) :
+			((x < l) ? (F_A * x + F_B * (a - x)) : 
+			(F_A * x + F_B * (a - x) + F * (x - l))
 			)
-		# caution: wenn mehrere x mit gleichem M_max exisiteren, wird nur eins ausgegeben
-		x_canidate = (F_A) / q
-		if x_canidate < a && x_canidate > 0 && abs(M(x_canidate)) > abs(M(x_max_biege_M[i][a_i]))
-			x_max_biege_M[i][a_i] = x_canidate
-		end
-		x_canidate = a
-		if abs(M(x_canidate)) > abs(M(x_max_biege_M[i][a_i]))
-			x_max_biege_M[i][a_i] = x_canidate
-		end
-		x_canidate = (F_A - F) / q
-		if x_canidate > a && x_canidate < l && abs(M(x_canidate)) > abs(M(x_max_biege_M[i][a_i]))
-			x_max_biege_M[i][a_i] = x_canidate
-		end
-		x_canidate = l
-		if abs(M(x_canidate)) > abs(M(x_max_biege_M[i][a_i]))
-			x_max_biege_M[i][a_i] = x_canidate
-		end
-		if false && a == 0.66 && F == 5*9.81 && A == 10*1*2+8*1*2
-			println(A)
-			println(q)
-			println(F)
-			println(F_A)
-			println(M_A)
-			println(F_B)
-			println(M_B)
-			println("-")
-			println(M(0.0))
-			println(M(0.5))
-			println(M(1.0))
-			println(M(0.735))
-			println(x_max_biege_M[i][a_i])
-			println(M(x_max_biege_M[i][a_i]))
-			println("----")
-		end
-		if x_max_biege_M[i][a_i] < 0 || x_max_biege_M[i][a_i] > l
-			throw(ArgumentError("out of bounds!"))
-		end
-		M_max[i][a_i] = M(x_max_biege_M[i][a_i])
-		σ_max[i][a_i] = abs.(M_max[i][a_i]) * 1000 / W
-		SF[i][a_i] = σ_grenze / σ_max[i][a_i]
-		if SF[i][a_max] > SF[i][a_i]
-			a_max = a_i
-		end
-		global a_i += 1
+		)
+
+	# caution: wenn mehrere x mit gleichem M_max exisiteren, wird nur eins ausgegeben
+	if abs(M_lcl(x)) > M_b_max
+		M_b_max = abs(M_lcl(x))
+		x_M_b_max = x
 	end
-
-	local F, ρ, A, l, W, E, σ_grenze, p1, p2, p3, p4, p5, p6 = experiments[i]
-	local q = ρ * A * 1e-3 * g # *l/l
-	local F_A, F_B, M_A, M_B = lagerreaktionen[i][a_max]
-	x_max_biege_M[i][a_max]
-	M_max[i][a_max]
-	σ_max[i][a_max]
-	SF[i][a_max]
-
-	print(file, p1)
-	print(file, ",")
-	print(file, p2)
-	print(file, ",")
-	print(file, p3)
-	print(file, ",")
-	print(file, p4)
-	print(file, ",")
-	print(file, p5)
-	print(file, ",")
-	print(file, p6)
-	print(file, ",")
-	print(file, F/g)
-	print(file, ",")
-	print(file, collect(0:0.02:(l+0.01))[a_max])
-	print(file, ",")
-	print(file, σ_max[i][a_max])
-	print(file, ",")
-	println(file, SF[i][a_max])
+	if false
+		println(M_lcl(0.0))
+		println(M_lcl(0.5))
+		println(M_lcl(1.0))
+		println(M_lcl(0.735))
+		println(M_b_max)
+		println(M_lcl(x_canidate))
+		println("----")
+	end
 end
 
-close(file)
+σ_b = M_b_max / W_y
+println(x_M_b_max)
--- Dokumentation/BiegungTorsionTräger/Balken-Rechner-Biegung.jl
+++ Dokumentation/BiegungTorsionTräger/Balken-Rechner-Biegung.jl
@@ -1,4 +1,4 @@
-g = 9.81
+g = 0
 
 E = 70000
 ρ = 2.7
@@ -45,6 +45,7 @@ I_z = I_z_I + (y_SP_I - y_SP)^2 * A_I + I_z_U_l + (y_SP_U_l - y_SP)^2 * A_U_l + 
 W_y = I_y / max(z_SP, wandstrke_I + hoehe_U - z_SP)
 
 a_values = 0.00:0.01:l
+a_values = [0.5]
 #a_value_s = [0.25,0.33,0.5,0.66,0.75]
 # maximale Biegespannung 
 M_b_max = 0
@@ -55,10 +56,10 @@ for a in a_values
 	global a_M_b_max
 	q = ρ * A * 1e-3 * g # *l/l
 	# lagerreaktionen
-	F_F_A = F * (1.0 - 3.0 * (a ^ 2) / (l^2) + 2.0 * (a ^ 3) / (l^3))
+	F_F_A = F * (1.0 - 3.0 * (a^2) / (l^2) + 2.0 * (a^3) / (l^3))
 	F_F_B = F - F_F_A
-	F_M_A = F * a * ((l - a) / l) ^ 2
-	F_M_B = F * (l - a) * (a / l) ^ 2
+	F_M_A = F * a * ((l - a) / l)^2
+	F_M_B = F * (l - a) * (a / l)^2
 	q = ρ * A * 1e-3 * g # *l/l
 	q_F_A = 0.5 * q * l
 	q_F_B = q_F_A
@@ -100,6 +101,7 @@ for a in a_values
 end
 
 σ_b = M_b_max * 1e3 / W_y
+println(a_M_b_max)
 
 # Annamhe: Rechtecksrohr # https://www.johannes-strommer.com/formeln/flaechentraegheitsmoment-widerstandsmoment/#formel_torsion
 A_t = 2.0 * (wandstrke_U * (hoehe_U + wandstrke_I)) + 2.0 * (min(wandstrke_I, wandstrke_U) * breite_U - min(wandstrke_I, wandstrke_U) * wandstrke_U * 2.0)
@@ -112,119 +114,11 @@ W_t = 2.0 * (breite_U - wandstrke_U) * (hoehe_U + wandstrke_I - wandstrke_U) * m
 
 σ_grenze = 160
 
-SF = σ_grenze/σ_GEH
+SF = σ_grenze / σ_GEH
 println(SF)
-fail()
 
-experiments = collect(
-	Iterators.flatten([
-		[
-			(FF, ρρ, AA, ll, WW, EE, σσ, p1, p2, p3, p4, p5, p6) for
-			(ρρ, AA, ll, WW, EE, σσ, p1, p2, p3, p4, p5, p6) in zip(ρ, A, l, W, E, σ_grenze, balken_parameters[:profile], balken_parameters[:source], balken_parameters[:A], balken_parameters[:B], balken_parameters[:C], balken_parameters[:D])
-		] for FF in F
-	]),
-)
-lagerreaktionen = Vector()
-x_max_biege_M = Vector()
-M_max = Vector()
-σ_max = Vector()
-SF = Vector()
-file = open(joinpath("C:\\Users\\Admin\\Nextcloud\\uni\\master\\projektmodul", "Blaken-SFs.csv"), "w+")
-println(file, "profil,quelle,A,B,C,D,Last,a_max,sigma_max,SF")
-for i in eachindex(experiments)
-	global a_i = 1
-	global a_max = 1
-	push!(lagerreaktionen, Vector())
-	push!(x_max_biege_M, zeros(length(experiments)))
-	push!(M_max, zeros(length(experiments)))
-	push!(σ_max, zeros(length(experiments)))
-	push!(SF, zeros(length(experiments)))
-	local F, ρ, A, l, W, E, σ_grenze = experiments[i]
-	for a in 0:0.02:(l+0.01)
-		push!(lagerreaktionen[i], get_lager_reaktionen((F, a, ρ, A, l)))
-		local q = ρ * A * 1e-3 * g # *l/l
-		local F_A, F_B, M_A, M_B = lagerreaktionen[i][a_i]
-		# always output abs max M 
-		M =
-			x -> (
-				(x > a) ? (F_A * x - M_A - q * x * x / 2 - F * (x - a)) :
-				((x < a) ? (F_A * x - M_A - q * x * x / 2) :
-				 ([F_A * x - M_A - q * x * x / 2, F_A * x - M_A - q * x * x / 2 - F * (x - a)][argmax(abs.([F_A * x - M_A - q * x * x / 2, F_A * x - M_A - q * x * x / 2 - F * (x - a)]))])
-				)
-			)
-		# caution: wenn mehrere x mit gleichem M_max exisiteren, wird nur eins ausgegeben
-		x_canidate = (F_A) / q
-		if x_canidate < a && x_canidate > 0 && abs(M(x_canidate)) > abs(M(x_max_biege_M[i][a_i]))
-			x_max_biege_M[i][a_i] = x_canidate
-		end
-		x_canidate = a
-		if abs(M(x_canidate)) > abs(M(x_max_biege_M[i][a_i]))
-			x_max_biege_M[i][a_i] = x_canidate
-		end
-		x_canidate = (F_A - F) / q
-		if x_canidate > a && x_canidate < l && abs(M(x_canidate)) > abs(M(x_max_biege_M[i][a_i]))
-			x_max_biege_M[i][a_i] = x_canidate
-		end
-		x_canidate = l
-		if abs(M(x_canidate)) > abs(M(x_max_biege_M[i][a_i]))
-			x_max_biege_M[i][a_i] = x_canidate
-		end
-		if false && a == 0.66 && F == 5 * 9.81 && A == 10 * 1 * 2 + 8 * 1 * 2
-			println(A)
-			println(q)
-			println(F)
-			println(F_A)
-			println(M_A)
-			println(F_B)
-			println(M_B)
-			println("-")
-			println(M(0.0))
-			println(M(0.5))
-			println(M(1.0))
-			println(M(0.735))
-			println(x_max_biege_M[i][a_i])
-			println(M(x_max_biege_M[i][a_i]))
-			println("----")
-		end
-		if x_max_biege_M[i][a_i] < 0 || x_max_biege_M[i][a_i] > l
-			throw(ArgumentError("out of bounds!"))
-		end
-		M_max[i][a_i] = M(x_max_biege_M[i][a_i])
-		σ_max[i][a_i] = abs.(M_max[i][a_i]) * 1000 / W
-		SF[i][a_i] = σ_grenze / σ_max[i][a_i]
-		if SF[i][a_max] > SF[i][a_i]
-			a_max = a_i
-		end
-		global a_i += 1
-	end
-
-	local F, ρ, A, l, W, E, σ_grenze, p1, p2, p3, p4, p5, p6 = experiments[i]
-	local q = ρ * A * 1e-3 * g # *l/l
-	local F_A, F_B, M_A, M_B = lagerreaktionen[i][a_max]
-	x_max_biege_M[i][a_max]
-	M_max[i][a_max]
-	σ_max[i][a_max]
-	SF[i][a_max]
-
-	print(file, p1)
-	print(file, ",")
-	print(file, p2)
-	print(file, ",")
-	print(file, p3)
-	print(file, ",")
-	print(file, p4)
-	print(file, ",")
-	print(file, p5)
-	print(file, ",")
-	print(file, p6)
-	print(file, ",")
-	print(file, F / g)
-	print(file, ",")
-	print(file, collect(0:0.02:(l+0.01))[a_max])
-	print(file, ",")
-	print(file, σ_max[i][a_max])
-	print(file, ",")
-	println(file, SF[i][a_max])
-end
-
-close(file)
+l=1000
+durchbiegung = a->(F * a^3.0 * (l - a)^3.0) / (3.0 * l^3.0 * E * I_y)
+durchbiegung_max = maximum(durchbiegung.(0:0.01:l))
+durchbiegung_500 = durchbiegung.(500)
+durchbiegung_667 = durchbiegung(670)
